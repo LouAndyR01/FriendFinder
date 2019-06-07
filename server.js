019c6258eecf324d9e02251398c56fcfb971a9b3
Louis Rodriguez <louisp285@yahoo.com>
2019-06-07 02:10:37 -0500

    serverready

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+//installing dependencies
 var express = require("express");
-var bodyParser = require("body-parser");
-var path
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//connection made
+// console.log("listening on PORT: " + PORT);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// applying the api and html routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+console.log("listening on PORT: " + PORT);
+});
+
