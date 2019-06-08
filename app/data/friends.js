9e8497ad219c0664e0fa8fad583b096adf730ec1
Louis Rodriguez <louisp285@yahoo.com>
2019-06-07 22:52:49 -0500

    friendserver

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+//create array of potential friends
+
+var friends = [
+    {   
+    name: "Will Smith",
+    photo: 'https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzQzMTY2NDc4/will-smith-9542165-1-402.jpg',
+    scores: [3, 1, 4, 4, 2, 4, 5, 2, 3, 5]
+}, { 
+    name: "Rob Gronkowski",
+    photo: "https://wgme.com/resources/media/e6d35edc-eb6a-434c-b32e-031ab6101829-large16x9_GronkAp.jpg?1553466582282",
+    scores: [1, 5, 5, 4, 5, 3, 4, 3, 1, 4]
+}, {    
+    name: "Emma Watson", 
+    photo: "https://www.imdb.com/name/nm0914612/mediaviewer/rm2697969664",
+    scores: [4, 1, 3, 1, 1, 2, 5, 4, 2, 1]
+}, {  
+    name: "Bruce Willis",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Bruce_Willis_by_Gage_Skidmore.jpg/220px-Bruce_Willis_by_Gage_Skidmore.jpg",
+    scores: [5, 2, 3, 3, 4, 1, 2, 5, 3, 3] 
+}, {
+    name: "Natalie Portman",
+    photo: "https://www.imdb.com/name/nm0000204/mediaviewer/rm512132096",
+    scores: [2, 2, 1, 3, 4, 2, 5, 2, 5, 4]
+}];  
+ 
+//export array
+module.exports = friends;  
